5f430fb289c3e01fb11d04b1c7a743067553b9bb
Hojin Koh <hojin-koh@users.noreply.github.com>
2021-10-10 22:07:44 +0800

    Do remember to add the "using"...

--- tests/hiter/iters.sub.h
+++ tests/hiter/iters.sub.h
@@ -7,10 +7,16 @@
 
 namespace test_hiter {
 
-  struct IterVecInt : public ::cppcfwv0::HIter<IterVecInt, int> {};
+  struct IterVecInt : public ::cppcfwv0::HIter<IterVecInt, int> {
+    using HIter::HIter;
+  };
 
-  struct IterSListStr : public ::cppcfwv0::HIter<IterSListStr, std::string> {};
+  struct IterSListStr : public ::cppcfwv0::HIter<IterSListStr, std::string> {
+    using HIter::HIter;
+  };
 
-  struct IterMapStrInt : public ::cppcfwv0::HIter<IterMapStrInt, std::pair<const std::string, int>> {};
+  struct IterMapStrInt : public ::cppcfwv0::HIter<IterMapStrInt, std::pair<const std::string, int>> {
+    using HIter::HIter;
+  };
 
 }
